73e8ba67227a5ad38eaf94b55a8414b9619b2758
Samuel Rodriguez <samuelroul@unisabana.edu.co>
2024-08-28 09:20:47 -0500

    no-responsive

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-function toggleShare() {
-    const shareOptions = document.getElementById('share-options');
-    shareOptions.style.display = shareOptions.style.display === 'flex' ? 'none' : 'flex';
-    if (shareOptions.style.display === 'flex') {
-      navigator.clipboard.writeText('https://dsaw-2024-2.github.io/parcial-i-sam200630/')
-        .then(() => console.log('Enlace copiado al portapapeles'))
-        .catch(err => console.error('Error al copiar el enlace: ', err));
+document.addEventListener('DOMContentLoaded', () => {
+  const shareButton = document.querySelector('.share-button');
+  const shareOptions = document.getElementById('share-options');
+
+  function toggleShare(event) {
+    event.stopPropagation(); // Prevenir que el evento se propague al documento
+
+    // Cambia entre agregar y quitar la clase 'hidden'
+    shareOptions.classList.toggle('hidden');
+  }
+
+  // Agregar event listener al botón de compartir
+  shareButton.addEventListener('click', toggleShare);
+
+  // Cierra el cuadro de compartir si se hace clic fuera de él
+  document.addEventListener('click', (event) => {
+    const isClickInside = shareOptions.contains(event.target) || shareButton.contains(event.target);
+
+    if (!isClickInside) {
+      shareOptions.classList.add('hidden');
     }
-  }  
+  });
+});
